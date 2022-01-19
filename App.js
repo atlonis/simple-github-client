1c3ef611d05c99cf625e9b781fc05e207e2b251c
Vlad Minkov <vladislav.minkov95@gmail.com>
2022-01-19 09:53:19 +0300

    Update metro config to support 'cjs' format and init ApolloProvider

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { GITHUB_GRAPHQL_URI } from '@env';
 
 import AppNavigator from './src/navigation/AppNavigator';
+
+const client = new ApolloClient({
+  uri: GITHUB_GRAPHQL_URI,
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <ApolloProvider client={client}>
+      <AppNavigator />
+    </ApolloProvider>
+  );
 };
 
 export default App;
