cca35aee6541378ea76b5b21e3b57ecb1a369625
Vlad Minkov <vladislav.minkov95@gmail.com>
2022-01-19 18:35:56 +0300

    Set Bearer token for apollo

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { GITHUB_GRAPHQL_URI } from '@env';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { GITHUB_GRAPHQL_URI, GITHUB_TOKEN } from '@env';
+import { setContext } from '@apollo/client/link/context';
 
 import AppNavigator from './src/navigation/AppNavigator';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: GITHUB_GRAPHQL_URI,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: GITHUB_TOKEN ? `Bearer ${GITHUB_TOKEN}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
